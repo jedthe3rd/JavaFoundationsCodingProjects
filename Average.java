faa3299cc1696a570168b7350f0979f06af63806
tariandeath <tariandeath210@gmail.com>
2016-09-08 11:13:53 -0600

    Initial Commit of Finished Programming Projects
    This is my initial commit of finished projects.

---
+++ Average.java
@@ -0,0 +1,20 @@
+/*
+@JEDIII
+*/
+import java.util.Scanner;
+
+public class Average {
+	public static void main (String[] args)
+	{
+		Scanner numbers = new Scanner(System.in);
+		System.out.println("Give me the three #'s you want to average?");
+		int one = numbers.nextInt();
+		int two = numbers.nextInt();
+		int three = numbers.nextInt();
+		
+		int average = (one + two + three)/3;
+		
+		System.out.println("Your #'s averaged: " + average);
+		
+	}
+}
---
+++ TempConverstion.java
@@ -0,0 +1,12 @@
+/*
+@JEDIII
+*/
+import java.util.Scanner;
+public class TempConversion{
+	public static void main(String[] args)
+	{
+		Scanner temp = new Scanner(System.in);
+		System.out.println("What temperature do you want me to convert?");
+		int temp = 
+	}
+}
