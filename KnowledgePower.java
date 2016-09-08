14ef5d2c675c0d321fa0e7a17cbe02c23d723d4d
tariandeath <tariandeath210@gmail.com>
2016-09-08 11:10:46 -0600

    Revert "Revert "This asks for 3 #'s and averages them.""
    This reverts commit 4639453d8fb3b889d18a2a2a9b94a0d8c5fd2452.

---
+++ KnowledgePower.java
@@ -0,0 +1,15 @@
+public class KnowledgePower
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Knowledge is Power");
+		
+		System.out.println("Knowledge");
+		System.out.println("is");
+		System.out.println("Power");
+		
+		System.out.println("========================");
+		System.out.println("|  Knowledge is Power  |");
+		System.out.println("========================");
+	}
+}
