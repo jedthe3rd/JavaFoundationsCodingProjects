4639453d8fb3b889d18a2a2a9b94a0d8c5fd2452
tariandeath <tariandeath210@gmail.com>
2016-09-08 11:10:04 -0600

    Revert "This asks for 3 #'s and averages them."
    This reverts commit 57becf4cfb95396a6434c017aa443e38ff656512.

--- FavSites.java
+++
@@ -1,7 +0,0 @@
-public class FavSites
-{
-	public static void main(String[] args)
-	{
-		System.out.println(" IFL Science: http://www.iflscience.com \n Udemy: https://www.udemy.com/ \n Pandora: https://www.pandora.com/ \n pcGamer: http://www.pcgamer.com/ \n Hexus: http://hexus.net/ \n LifeHacker: http://lifehacker.com");
-	}
-}
