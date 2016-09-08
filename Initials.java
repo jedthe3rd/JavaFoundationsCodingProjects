57becf4cfb95396a6434c017aa443e38ff656512
tariandeath <tariandeath210@gmail.com>
2016-09-08 11:09:43 -0600

    This asks for 3 #'s and averages them.

---
+++ Initials.java
@@ -0,0 +1,15 @@
+public class Initials
+{
+	public static void main(String[] args)
+	{
+		System.out.println("  JJJJJJJJJJJ     EEEEEEEEE     DDDDDD");
+		System.out.println("  JJJJJJJJJJJ     EEEEEEEEE     DDDDDDDD");
+		System.out.println("       JJJ        EEE           DD      DD");
+		System.out.println("       JJJ        EEE           DD      DD");
+		System.out.println("       JJJ        EEEEEEEEE     DD      DD");
+		System.out.println("JJJ   JJJ         EEE           DD      DD");
+		System.out.println("JJ    JJJ         EEE           DD      DD");
+		System.out.println(" JJJJJJJJ         EEEEEEEEE     DDDDDDDD");
+		System.out.println("  JJJJJ           EEEEEEEEE     DDDDDD");
+	}
+}
